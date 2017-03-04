751f776dbec9914209c06c26cb4bb13da9bc3073
Jacob Klegar <jklegar@Jacobs-MacBook-Air-4.local>
2017-03-04 14:11:37 -0500

    smaller data set

--- classification_starter.py
+++ classification_starter.py
@@ -100,7 +100,8 @@ def extract_feats(ffs, direc="train", global_feat_dict=None):
     fds = [] # list of feature dicts
     classes = []
     ids = [] 
-    for datafile in os.listdir(direc):
+    testNum = 500
+    for datafile in os.listdir(direc)[0:testNum]:
         # extract id and true class (if available) from filename
         id_str,clazz = datafile.split('.')[:2]
         ids.append(id_str)
